7945c9bf6a9ef00e1f1fcb67c3f28d85fa12a5a8
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-11-08 09:47:08 +0600

    user & player

--- player.rb
+++ player.rb
@@ -1,9 +1,17 @@
 class Player
-  attr_writer :money
-  attr_reader :name
+  attr_writer :money, :cards
+  attr_reader :name, :points
 
   def initialize(name)
     @name = name
     @money = 100
+    @cards = []
+    @points = 0
+  end
+
+  def count_points
+    # посчитать сумму очков
+    # добавить логику туза, если прибавить 11 и сумма выше 21 то прибавлять 1
+    @cards.each { |card| @points += card[:points] }
   end
 end
--- user.rb
+++ user.rb
@@ -1,5 +1,7 @@
 require_relative 'player'
 
 class User < Player
-
+  #Пропустить
+  #Добавить карту
+  #Открыть карты
 end
