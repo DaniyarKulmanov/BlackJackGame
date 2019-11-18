c506724a82e9da31d54b759f633a0182bd5cfcb6
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-11-18 09:23:18 +0600

    21 count refactor

--- game.rb
+++ game.rb
@@ -4,46 +4,8 @@ require_relative 'deck'
 
 class Game
 
-  def self.count_points(player)
-    count_simple player
-    count_aces player
-  end
-
-  def self.count_simple(player)
-    simple_cards = player.cards.select { |card| card[:alter_points].nil? }
-    simple_cards.each { |card| player.points += card[:points] }
-  end
-
-  def self.count_aces(player)
-    aces_cards = player.cards.select { |card| ACES.include? card[:card] }
-    # puts aces_cards
-    sum1 = 0
-    sum2 = 0
-    sum3 = 0
-    sums = []
-    aces_cards.each do |ace|
-      sum3 = sum1
-      sum1 += ace[:points]
-      sum2 += ace[:alter_points]
-      sum3 += ace[:alter_points]
-      # if player.points + ace[:alter_points] >= 21
-      #   player.points = player.points - ace[:alter_points] + ace[:points]
-      # else
-      #   player.points += ace[:alter_points]
-      # end
-    end
-    sums = [sum1, sum2, sum3]
-    sums.map! { |sum| sum += player.points }
-    puts sums
-    puts "nearest:"
-    sums.sort!
-    player.points = sums.select { |sum| sum <= 21 }.last
-    # check if sum >=21 and 2 or 3 aces
-    puts "player pts - #{player.points}"
-  end
-
   def initialize(user)
-    @user = user
+    @player = user
     @dealer = Dealer.new
     @cards ||= []
     @bank = 0
@@ -64,13 +26,14 @@ class Game
 
   private
 
-  attr_accessor :user, :dealer, :cards, :bank
+  attr_accessor :player, :dealer, :cards, :bank, :sums
 
   def prepare_round
     @cards = Deck.new.cards
-    take cards, user
+    @sums = []
+    take cards, player
     take cards, dealer
-    make_bet user
+    make_bet player
     make_bet dealer
   end
 
@@ -85,17 +48,17 @@ class Game
   end
 
   def no_money?
-    user.money.zero? || dealer.money.zero?
+    player.money.zero? || dealer.money.zero?
   end
 
   def lost
-    puts "#{user.name} #{LOSE}"
+    puts "#{player.name} #{LOSE}"
     (1..5).each { print "#{SORRY} " }
     puts ''
   end
 
   def end_game
-    puts "#{FAREWELL} #{username}"
+    puts "#{FAREWELL} #{player.name}"
   end
 
   def take(cards, player)
@@ -112,18 +75,41 @@ class Game
   end
 
   def show_result
-    count_points user
+    count_points player
     count_points dealer
+    print_result
   end
 
-
-
-  def aces_points(sums, player)
-    # nearest21 = sums.sort.group_by{ |points| points <=> 21 }
-    # fix_sum = nearest21[-1].last || nearest21[1].first
-    # player.points = fix_sum
-    a = 21 - sums[0]
-    b = 21 - sums[1]
-    nearest21 = a > b ? a : b
+  def count_points(player)
+    count_simple_cards player
+    count_aces player
   end
+
+  def count_simple_cards(player)
+    simple_cards = player.cards.select { |card| card[:alter_points].nil? }
+    simple_cards.each { |card| player.points += card[:points] }
+  end
+
+  def count_aces(player)
+    aces_cards = player.cards.select { |card| ACES.include? card[:card] }
+    collect_possible_sums aces_cards
+    @sums.map! { |sum| sum += player.points }
+    @sums.sort!
+    player.points = @sums.select { |sum| sum <= 21 }.last
+  end
+
+  def collect_possible_sums(cards)
+    if cards.empty?
+      sums = []
+      cards.each do |ace|
+        sums[2] = sum1
+        sums[0] += ace[:points]
+        sums[1] += ace[:alter_points]
+        sums[2] += ace[:alter_points]
+      end
+      @sums = sums
+    end
+  end
+
+  def print_result; end
 end
