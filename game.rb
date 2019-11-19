20b27d21b093e1723a5b0f1291c835def49b25ad
DaniyarKulmanov <danrubydeveloper@gmail.com>
2019-11-19 12:14:28 +0600

    moved point counts to player.rb

--- game.rb
+++ game.rb
@@ -26,7 +26,7 @@ class Game
 
   private
 
-  attr_accessor :player, :dealer, :cards, :bank, :sums
+  attr_accessor :player, :dealer, :cards, :bank
 
   def prepare_round
     init_game_attributes
@@ -65,46 +65,13 @@ class Game
   end
 
   def show_result
-    count_points player
-    count_points dealer
+    player.count_points
+    dealer.count_points
     print_result
-  end
-
-  def count_points(player)
-    player.points = 0
-    count_simple_cards player
-    count_aces player
-  end
-
-  def count_simple_cards(player)
-    simple_cards = player.cards.select { |card| card[:alter_points].nil? }
-    simple_cards.each { |card| player.points += card[:points] }
-  end
-
-  def count_aces(player)
-    aces_cards = player.cards.select { |card| ACES.include? card[:card] }
-    @sums = possible_sums aces_cards unless aces_cards.empty?
-    unless aces_cards.empty?
-      @sums.map! { |sum| sum += player.points }
-      @sums.sort!
-      player.points = @sums.select { |sum| sum <= 21 }.last
-    end
-  end
-
-  def possible_sums(cards)
-    sums = [0, 0, 0]
-    cards.each do |ace|
-      sums[2] = sums[0]
-      sums[0] += ace[:points]
-      sums[1] += ace[:alter_points]
-      sums[2] += ace[:alter_points]
-    end
-    sums
   end
 
   def init_game_attributes
     @cards = Deck.new.cards
-    @sums = []
     player.cards = []
     dealer.cards = []
     2.times { give_card player }
--- player.rb
+++ player.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_accessor :money, :cards, :points
+  attr_accessor :money, :cards, :points, :sums
   attr_reader :name, :gender
 
   def initialize(name, gender)
@@ -12,5 +12,38 @@ class Player
 
   def take(cards)
     @cards.push cards.sample
+  end
+
+  def count_points
+    @points = 0
+    count_simple_cards
+    count_aces
+  end
+
+  private
+
+  def count_simple_cards
+    simple_cards = cards.select { |card| card[:alter_points].nil? }
+    simple_cards.each { |card| @points += card[:points] }
+  end
+
+  def count_aces
+    aces_cards = cards.select { |card| ACES.include? card[:card] }
+    possible_sums aces_cards unless aces_cards.empty?
+    unless aces_cards.empty?
+      @sums.map! { |sum| sum += player.points }
+      @sums.sort!
+      player.points = @sums.select { |sum| sum <= 21 }.last
+    end
+  end
+
+  def possible_sums(cards)
+    @sums = [0, 0, 0]
+    cards.each do |ace|
+      @sums[2] = @sums[0]
+      @sums[0] += ace[:points]
+      @sums[1] += ace[:alter_points]
+      @sums[2] += ace[:alter_points]
+    end
   end
 end
